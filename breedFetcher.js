937dd7da69e24343a563c26254adaf3533c274b5
Jacky Lau <jackylau0126@gmail.com>
2019-09-26 22:39:00 +0000

    implemented call back functions; pair programming with darren

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,26 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
-const breed = args[0];
+// const args = process.argv.slice(2);
+// const breed = args[0];
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    
+    if (error) {
+      callback(error, null);
+    } else {
+      if (body === "[]") {
+        //console.log("Breed Not Found");
+        callback("Breed Not Found", null)
+      } else if (body !== "[]") {
+        const data = JSON.parse(body);
+        //console.log(data[0]["description"]);
+        callback(null, data[0]["description"]);
+      }
+    }
+  });
+};
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  
-  if (error) {
-    console.log("Request Failed")
-    throw Error;
-  } else {
-  if (body === "[]") {
-      console.log("Breed Not Found");
-    } else if (body !== "[]") {
-      const data = JSON.parse(body);
-      console.log(data[0]["description"]);
-    };
-  }
-});
+
+module.exports = { fetchBreedDescription };
